61c55b60fdd35da4ef420a5f1348aa538270b77a
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-25 00:16:30 +0530

    add error page

---
+++ src/Components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className='flex w-[100%] h-[100%] items-center justify-center font-mono text-center p-10'>
+        Error: The page you landed on does not exist.
+
+    </div>
+  )
+}
+
+export default Error
