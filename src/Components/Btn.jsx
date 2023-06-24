ee28d6dd0872c15783f1c0769e1d8b4805801f7d
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-25 01:14:51 +0530

    add select page

---
+++ src/Components/Btn.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function Btn(props) {
+  /*   const [isButtonDisabled, setIsButtonDisabled] = useState(false)*/
+  const selectedColor =
+    "bg-indigo-500 hover:bg-indigo-300 active:bg-indigo-500";
+  const defaultColor = "bg-[#FFFDD0] hover:bg-indigo-200 active:bg-indigo-400";
+  const {
+    setSelectedFields,
+    selectedFields,
+    setSelectedButtonNames,
+    selectedButtonName,
+  } = props;
+
+  const handleClick = () => {
+    if (selectedFields.has(props.name)) {
+      setSelectedFields((previousFields) => {
+        previousFields.delete(props.name);
+        return new Set(previousFields.values());
+      });
+      setSelectedButtonNames((previousNames) =>
+        previousNames.filter((n) => n !== props.name)
+      );
+    } else {
+      setSelectedFields((previousFields) => {
+        previousFields.add(props.name);
+        return new Set(previousFields.values());
+      });
+      setSelectedButtonNames((previousNames) => [...previousNames, props.name]);
+    }
+  };
+
+  const bgColor = selectedFields.has(props.name) ? selectedColor : defaultColor;
+  const isButtonDisabled =
+    !selectedFields.has(props.name) && selectedFields.size > 2;
+
+  return (
+    <div>
+      <button
+        className={`flex justify-around ${bgColor}  text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-48 sm:w-48 lg:w-72 mb-5  cursor-pointer`}
+        onClick={handleClick}
+        disabled={isButtonDisabled}
+      >
+        <span className="inline-block">{props.name}</span>
+      </button>
+    </div>
+  );
+}
+
+export default Btn;
---
+++ src/Components/Select.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Buttons";
+import { useAuth0 } from "@auth0/auth0-react";
+import Btn from "./Btn";
+
+function Select(props) {
+  const [selectedFields, setSelectedFields] = useState(new Set());
+  const [selectedButtonNames, setSelectedButtonNames] = useState([]);
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isAuthenticated) {
+    console.log(user.email);
+    console.log(user.email);
+    console.log(user.email);
+  }
+
+  function Fetch() {
+    const data = {
+      email: user.email,
+      interests: selectedButtonNames,
+    };
+
+    fetch("http://localhost:5050", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        // Handle the response data
+        console.log(responseData);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error(error);
+      });
+  }
+
+  const { button } = props;
+  return (
+    <div className="flex flex-col h-[100%] w-[100%] justify-around items-center p-5">
+      <div>
+        <div className="font-roboto_bold text-2xl pb-8">
+          I want to read about...
+        </div>
+        <div className="font-roboto text-xs sm:text-xs lg:text-md font-semibold  pb-3 text-center">
+          Please choose any three of your interests
+        </div>
+      </div>
+
+      <div className=" flex flex-col items-center w-[90%] h-[50%] overflow-y-scroll mb-10 scrollbar-thin scrollbar-thumb-indigo-500 scrollbar-thumb-rounded-lg scrollbar-track-rounded-lg scrollbar-track-indigo-200">
+        {props.button.map((el) => {
+          return (
+            <Btn
+              key={el.key}
+              name={el.name}
+              selectedFields={selectedFields}
+              setSelectedFields={setSelectedFields}
+              selectedButtonNames={selectedButtonNames}
+              setSelectedButtonNames={setSelectedButtonNames}
+            />
+          );
+        })}
+      </div>
+
+      <Link to="/final">
+        <button
+          className="h-10 w-52 text-xl font-roboto bg-indigo-300 hover:bg-indigo-400 cursor-pointer rounded-md xl border border-x-1 border-y-1 border-solid border-[#16161d] shadow-xl text-white shadow-indigo-500"
+          onClick={Fetch}
+        >
+          Select
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default Select;
