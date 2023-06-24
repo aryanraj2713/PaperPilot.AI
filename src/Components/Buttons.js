12e9048f3f8620b3a59f599c497cb4e3e6d7f1d0
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-25 00:00:05 +0530

    add Button.js

---
+++ src/Components/Buttons.js
@@ -0,0 +1,58 @@
+const button = [
+    {
+        key: 1,
+        name: "Security and Privacy"
+    },
+    {
+        key: 2,
+        name: "Robotics"
+    },
+    {
+        key: 3,
+        name: "Mathematics"
+    },
+    {
+        key: 4,
+        name: "Physics"
+    }, {
+        key: 5,
+        name: "Chemistry"
+    }, {
+        key: 6,
+        name: "Biology"
+    }, {
+        key: 7,
+        name: "Computer Science"
+    }, {
+        key: 8,
+        name: "Machine Learning"
+    }, {
+        key: 9,
+        name: "Artificial Intelligence"
+    }, {
+        key: 10,
+        name: "Quantum"
+    }, {
+        key: 11,
+        name: "Electrical"
+    }, {
+        key: 12,
+        name: "Mechanical"
+    }, {
+        key: 13,
+        name: "Civil"
+    }, {
+        key: 14,
+        name: "Arts"
+    }, {
+        key: 15,
+        name: "Design"
+    }, {
+        key: 16,
+        name: "Architecture"
+    }, {
+        key: 17,
+        name: "Power"
+    },
+
+];
