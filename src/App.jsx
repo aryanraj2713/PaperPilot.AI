3b7de17ed85597b0259954963d8c5213be8387b2
zaid <zaid.tab123@gmail.com>
2023-06-25 10:25:29 +0530

    🐛 fx auth0

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./Components/Landing";
 import Error from "./Components/Error";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createAuth0Client } from "@auth0/auth0-spa-js";
+
 function App() {
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENTID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>{" "}
+  </React.StrictMode>
+);
